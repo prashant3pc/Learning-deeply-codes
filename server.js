d7136942f77c28b29c37c4390f656cb568454525
prashant3pc <123456789prashant123456789@gmail.com>
2026-09-07 21:14:55 +0545

    updated authentication

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
+import myMiddleware from "./middlleware/logger.js";
 
 const app = express();
 app.use(express.json());
 
+app.use(myMiddleware);
 app.use(userRoutes);
 
 connectDB();
