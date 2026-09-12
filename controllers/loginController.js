3dca1462e717122389d5359ee88596516fbf4540
prashant3pc <chaudharypuspa502@gmail.com>
2026-09-13 00:17:57 +0545

    auth completed

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -22,4 +23,15 @@ export const loginUser = asyncHandler(async (req, res) => {
       message: "Invalid Credentials",
     });
   }
+
+  const createToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  res.json({
+    success: true,
+    data: createToken,
+    message: "Token created",
+  });
 });
+
+//   jwt.verify(createToken, process.env.JWT_SECRET);
